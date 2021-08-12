0e5c577ccf9358fd5b2d2b78a619d3ea894a51d5
xiaodeng <927561237@qq.com>
2021-08-12 16:29:25 +0800

    外观和样式调整

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 //导入 antd-mobile 样式
 import 'antd-mobile/dist/antd-mobile.css'
+// 我们自己写的全局样式要放在组件库样式的后面 后面的样式会覆盖前面的样式
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
