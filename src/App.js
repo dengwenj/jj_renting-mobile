1b4c8d073f184e5d10864f17c729edefcb7468df
xiaodeng <927561237@qq.com>
2021-08-12 16:22:33 +0800

    配置基本路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+import Home from './pages/Home'
+import CityList from './pages/CityList'
 
 export default function App() {
-  return <div>app...</div>
+  return (
+    <BrowserRouter>
+      {/* 配置导航菜单 */}
+      <Link to="/home">首页</Link>
+      <Link to="/citylist">城市选择</Link>
+
+      {/* 配置路由 */}
+      <Route path="/home" component={Home} />
+      <Route path="/citylist" component={CityList} />
+    </BrowserRouter>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+//导入 antd-mobile 样式
+import 'antd-mobile/dist/antd-mobile.css'
 
 ReactDOM.render(
   <React.StrictMode>
