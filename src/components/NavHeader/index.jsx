7bd06c2e08be93e9425fed24c2f18d706487326e
xiaodeng <927561237@qq.com>
2021-08-14 20:43:51 +0800

    封装的NavHeader默认点击行为和不是默认点击行为

--- src/components/NavHeader/index.jsx
+++ src/components/NavHeader/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import { NavBar } from 'antd-mobile'
 import './index.scss'
 
-export default function NavHeader({ children }) {
+function NavHeader(props) {
+  // 默认点击行为 如果传了的话就按自己点击行为来做
+  const goBack = () => props.history.goBack()
+
   return (
     <NavBar
       className="navbar"
       mode="light"
       icon={<i className="iconfont icon-back" />}
-      onLeftClick={() => this.props.history.goBack()}
+      onLeftClick={props.onLeftClick || goBack}
     >
-      {children}
+      {props.children}
     </NavBar>
   )
 }
+export default withRouter(NavHeader)
