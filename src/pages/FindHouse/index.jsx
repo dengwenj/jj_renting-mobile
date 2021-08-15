019e416cd10a9c1f9815def3fed038e5de6bdab8
xiaodeng <927561237@qq.com>
2021-08-16 01:29:30 +0800

    找房模块搜索导航栏的样式设置

--- src/pages/FindHouse/index.jsx
+++ src/pages/FindHouse/index.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import SearchHeader from '@components/SearchHeader'
 import { getItem } from '@utils/storage'
+import './index.scss'
 
 export default class FindHouse extends Component {
   render() {
     const { label } = getItem('jjzf')
     return (
-      <div>
-        <SearchHeader cityInfo={label} />
+      <div className="find_house">
+        <div className="top">
+          <i
+            className="iconfont icon-back"
+            onClick={() => this.props.history.goBack()}
+          ></i>
+          <SearchHeader cityInfo={label} />
+        </div>
       </div>
     )
   }
