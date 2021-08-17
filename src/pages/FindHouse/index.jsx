8e480964a7a95b8a8933173a730b87297803b792
xiaodeng <927561237@qq.com>
2021-08-17 19:47:38 +0800

    进入页面时获取数据

--- src/pages/FindHouse/index.jsx
+++ src/pages/FindHouse/index.jsx
@@ -7,9 +7,18 @@ import './index.scss'
 
 export default class FindHouse extends Component {
   state = {
-    start: 1,
-    end: 10,
+    start: 1, // 页码
+    end: 10, // 每页多少条数据
+    list: [], // 列表数据
+    count: null, // 总共获取的数据
   }
+
+  // 挂载完毕调用的钩子
+  componentDidMount() {
+    // 调用 searchHouseList 获取数据
+    this.searchHouseList({})
+  }
+
   searchHouseList = async (filters) => {
     const { value } = getItem('jjzf')
     const { start, end } = this.state
@@ -17,7 +26,10 @@ export default class FindHouse extends Component {
     filters.end = end
     filters.cityId = value
     const res = await getHousesList(filters)
-    console.log(res)
+    this.setState({
+      list: res.data.body.list,
+      count: res.data.body.count,
+    })
   }
 
   render() {
