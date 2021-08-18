0de5de4f26b10d9b0a5c4392ee7419fbb045886e
xiaodeng <927561237@qq.com>
2021-08-18 23:05:11 +0800

    使用formik重构登录功能

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Flex, WingBlank, WhiteSpace, Toast } from 'antd-mobile'
+import { withFormik } from 'formik'
 
 import { Link } from 'react-router-dom'
 
@@ -16,7 +17,7 @@ import { setItem } from '@utils/storage'
 // const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
 
 class Login extends Component {
-  state = {
+  /* state = {
     username: '',
     password: '',
   }
@@ -54,10 +55,11 @@ class Login extends Component {
       return
     }
     Toast.info('账号或者密码错误')
-  }
+  } */
 
   render() {
-    const { username, password } = this.state
+    // const { username, password } = this.state
+    const { values, handleSubmit, handleChange } = this.props
     return (
       <div className={styles.root}>
         {/* 顶部导航 */}
@@ -66,14 +68,14 @@ class Login extends Component {
 
         {/* 登录表单 */}
         <WingBlank>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className={styles.formItem}>
               <input
                 className={styles.input}
                 name="username"
                 placeholder="请输入账号"
-                value={username}
-                onChange={this.handleUsername}
+                value={values.username}
+                onChange={handleChange}
               />
             </div>
             {/* 长度为5到8位，只能出现数字、字母、下划线 */}
@@ -84,8 +86,8 @@ class Login extends Component {
                 name="password"
                 type="password"
                 placeholder="请输入密码"
-                value={password}
-                onChange={this.handlePassword}
+                value={values.password}
+                onChange={handleChange}
               />
             </div>
             {/* 长度为5到12位，只能出现数字、字母、下划线 */}
@@ -107,4 +109,39 @@ class Login extends Component {
   }
 }
 
-export default Login
+/* 
+  1 导入 withFormik，使用 withFormik 高阶组件包裹 Login 组件
+  2 为 withFormik 提供配置对象：mapPropsToValues 和 handleSubmit
+  3 在 Login 组件中，通过 props 获取到 values（表单元素对象），handleSubmit，handleChange
+  4 使用 values 提供的值，设置为表单元素的 value，使用 handleChange 设置为表单元素的 onChange
+        在给表单元素设置 handleChange 的时候，为了让其生效，需要给表单元素添加 name 属性，并且 name 属性的值与当前 value 名称相同
+  5 使用 handleSubmit 设置为表单的 onSubmit
+  6 在 handleSubmit 中，通过 values 获取到表单元素值
+  7 在 handleSubmit 中，完成登录逻辑
+*/
+
+// withFormik 是个高阶组件
+export default withFormik({
+  // 提供的状态 为表单元素提供状态
+  mapPropsToValues: () => ({ username: '', password: '' }),
+  // 提供的方法 表单的提交事件
+  handleSubmit: async (values, { props }) => {
+    console.log(values)
+    const { username, password } = values
+    const res = await login({
+      username,
+      password,
+    })
+
+    const { status } = res.data
+
+    if (status === 200) {
+      // 说明登录通过
+      setItem('jjzf_token', res.data.body.token)
+      props.history.replace('/my')
+      Toast.success('登录成功', 2)
+      return
+    }
+    Toast.info('账号或者密码错误')
+  },
+})(Login)
