3add4420836f44435ef18a50593bd61e65cb6648
xiaodeng <927561237@qq.com>
2021-08-20 01:02:16 +0800

    路由懒加载

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import LayOut from './pages/LayOut'
-import CityList from './pages/CityList'
-import Search from './pages/Search'
-import Map from './pages/Map'
-import Rent from './pages/Rent'
-import RentAdd from './pages/Rent/Add'
-import RentSearch from './pages/Rent/Search'
-import HouseDetail from './pages/HouseDetail'
-import Login from './pages/Login'
-import Registe from './pages/Registe'
+// import LayOut from './pages/LayOut'
+// import CityList from './pages/CityList'
+// import Search from './pages/Search'
+// import Map from './pages/Map'
+// import Rent from './pages/Rent'
+// import RentAdd from './pages/Rent/Add'
+// import RentSearch from './pages/Rent/Search'
+// import HouseDetail from './pages/HouseDetail'
+// import Login from './pages/Login'
+// import Registe from './pages/Registe'
 
-import AuthRoute from '@components/AuthRoute'
+// import AuthRoute from '@components/AuthRoute'
+
+// 路由懒加载
+const LayOut = lazy(() => import('./pages/LayOut'))
+const Rent = lazy(() => import('./pages/Rent'))
+const RentAdd = lazy(() => import('./pages/Rent/Add'))
+const RentSearch = lazy(() => import('./pages/Rent/Search'))
+const Registe = lazy(() => import('./pages/Registe'))
+const Login = lazy(() => import('./pages/Login'))
+const HouseDetail = lazy(() => import('./pages/HouseDetail'))
+const Search = lazy(() => import('./pages/Search'))
+const Map = lazy(() => import('./pages/Map'))
+const CityList = lazy(() => import('./pages/CityList'))
+const AuthRoute = lazy(() => import('@components/AuthRoute'))
 
 export default function App() {
   return (
@@ -20,19 +33,21 @@ export default function App() {
       <BrowserRouter>
         {/* 配置路由 */}
         {/* 一级路由 */}
-        <Switch>
-          <AuthRoute exact path="/rent" component={Rent} />
-          <AuthRoute exact path="/rent/add" component={RentAdd} />
-          <AuthRoute exact path="/rent/search" component={RentSearch} />
-          <Route exact path="/registe" component={Registe} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/detail/:id" component={HouseDetail} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/map" component={Map} />
-          <Route exact path="/citylist" component={CityList} />
-          {/* 这个要写在后面 */}
-          <Route path="/" component={LayOut} />
-        </Switch>
+        <Suspense fallback={<div>loading</div>}>
+          <Switch>
+            <AuthRoute exact path="/rent" component={Rent} />
+            <AuthRoute exact path="/rent/add" component={RentAdd} />
+            <AuthRoute exact path="/rent/search" component={RentSearch} />
+            <Route exact path="/registe" component={Registe} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/detail/:id" component={HouseDetail} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/map" component={Map} />
+            <Route exact path="/citylist" component={CityList} />
+            {/* 这个要写在后面 */}
+            <Route path="/" component={LayOut} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </div>
   )
--- src/pages/LayOut/index.jsx
+++ src/pages/LayOut/index.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import { TabBar } from 'antd-mobile'
 
-import Home from '../Home'
-import FindHouse from '../FindHouse'
-import News from '../News'
-import My from '../My'
+// import Home from '../Home'
+// import FindHouse from '../FindHouse'
+// import News from '../News'
+// import My from '../My'
 import tabBarItem from '../../utils/tabBarItem'
 import './index.scss'
+
+// 路由懒加载
+const Home = lazy(() => import('../Home'))
+const FindHouse = lazy(() => import('../FindHouse'))
+const News = lazy(() => import('../News'))
+const My = lazy(() => import('../My'))
 
 export default class LayOut extends Component {
   state = {
@@ -52,13 +58,15 @@ export default class LayOut extends Component {
         {/* 二级路由  二级路由也要从一级路由这里下来 先要一级路由在二级路由 所有上哪里的拦截器就进不到这里 */}
         {/* 子路由 因为我在父路由里面写的 pathname 是 / 所以这里的子路由可以这样写 /news*/}
         {/* 这个必须要包 Switch 如果不包的话就要往下面找 就会导致 layOut 组件那种情况 就是找到 news 路径了 还往下找，就又去重定向 /home 了 */}
-        <Switch>
-          <Redirect exact from="/" to="/home" />
-          <Route path="/home" component={Home} />
-          <Route path="/findhouse" component={FindHouse} />
-          <Route path="/news" component={News} />
-          <Route path="/my" component={My} />
-        </Switch>
+        <Suspense fallback={<div>loading</div>}>
+          <Switch>
+            <Redirect exact from="/" to="/home" />
+            <Route path="/home" component={Home} />
+            <Route path="/findhouse" component={FindHouse} />
+            <Route path="/news" component={News} />
+            <Route path="/my" component={My} />
+          </Switch>
+        </Suspense>
 
         {/* tabbar */}
         <TabBar tintColor="#21b979" barTintColor="white">
